908087b450792445a161c5d61929fc7f6b55f691
Toby Bell <tbell@stanford.edu>
2024-08-01 00:03:32 -0700

    Remove header

--- include/dragonbox/dragonbox.h
+++ include/dragonbox/dragonbox.h
@@ -1,23 +1,4 @@
-// Copyright 2020-2024 Junekey Jeon
-//
-// The contents of this file may be used under the terms of
-// the Apache License v2.0 with LLVM Exceptions.
-//
-//    (See accompanying file LICENSE-Apache or copy at
-//     https://llvm.org/foundation/relicensing/LICENSE.txt)
-//
-// Alternatively, the contents of this file may be used under the terms of
-// the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE-Boost or copy at
-//     https://www.boost.org/LICENSE_1_0.txt)
-//
-// Unless required by applicable law or agreed to in writing, this software
-// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.
-
-
-#ifndef JKJ_HEADER_DRAGONBOX
-#define JKJ_HEADER_DRAGONBOX
+#pragma once
 
 // Attribute for storing static data into a dedicated place, e.g. flash memory. Every ODR-used
 // static data declaration will be decorated with this macro. The users may define this macro,
@@ -4084,6 +4065,4 @@ namespace jkj {
     #undef JKJ_STATIC_DATA_SECTION_DEFINED
 #else
     #undef JKJ_STATIC_DATA_SECTION
-#endif
-
 #endif
